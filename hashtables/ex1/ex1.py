d2828b8cda67ef1593875de4e206468f64c880e1
Pav0l <pavol.karas@gmail.com>
2019-07-12 11:15:56 +0200

    Get indices of item weights

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,19 +1,40 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    if length <= 1:
+        return None
 
-    """
-    YOUR CODE HERE
-    """
+    ht = HashTable(length)
 
-    return None
+    # add all elements from weights arr to dict
+    for i in range(length):
+        # check if a key (element from weights arr) is already in hash table
+        # (in case there are same elements in weights array)
+        key_exist = hash_table_retrieve(ht, weights[i])
+        if (key_exist == 0 or key_exist) and (limit - weights[i] == weights[i]):
+            # 2 same elements, each equal to limit/2 => element + element = limit
+            return (i, key_exist)
+        hash_table_insert(ht, weights[i], i)
+
+    # placeholder for keys that sum to limit
+    keys = []
+
+    # search for key, which adds to limit with another key => limit-key = another key
+    for i in range(length):
+        key_value = hash_table_retrieve(ht, limit-weights[i])
+        if key_value:
+            keys.append(weights[i])
+            keys.append(limit-weights[i])
+            break
+
+    indexes = [weights.index(keys[0]), weights.index(keys[1])]
+    indexes.sort(reverse=True)
+
+    return (indexes[0], indexes[1])
 
 
 def print_answer(answer):
